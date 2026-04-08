87e5e378b4b9f1d564da95d6b2541ac5887a89e0
FelipeDesk <felipedesk467@gmail.com>
2026-04-08 18:11:19 -0300

    função de editar pensamento com PUT

--- js/api.js
+++ js/api.js
@@ -4,7 +4,7 @@ const api = {
             const response = await fetch('http://localhost:3000/pensamentos')
             return await response.json()
         }
-        catch {
+        catch (error) {
             alert('Erro ao buscar pensamentos')
             throw error
         }
@@ -21,8 +21,36 @@ const api = {
             })
             return await response.json()
         }
-        catch {
-            alert('Erro ao buscar pensamentos')
+        catch (error) {
+            alert('Erro ao salvar pensamento')
+            throw error
+        }
+    },
+
+    async buscarPensamentoPorId(id) {
+        try {
+            const response = await fetch(`http://localhost:3000/pensamentos/${id}`)
+            return await response.json()
+        }
+        catch (error) {
+            alert('Erro ao buscar pensamento')
+            throw error
+        }
+    },
+
+    async editarPensamento(pensamento) {
+        try {
+            const response = await fetch(`http://localhost:3000/pensamentos/${pensamento.id}`, {
+                method: "PUT",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(pensamento)
+            })
+            return await response.json()
+        }
+        catch (error) {
+            alert('Erro ao editar pensamentos')
             throw error
         }
     }
